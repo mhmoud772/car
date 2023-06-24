32d0b1c96a88e53810b806f96941d5d53be5054a
mhmoud772 <137617055+mhmoud772@users.noreply.github.com>
2023-06-24 15:01:25 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CAR',
+    'summary': """This module will add a arachive to store CAR details""",
+    'version': '10.0.1.0.0',
+    'description': """This module will add a arachive to store CAR details""",
+    'author': 'Mahmoud Ahmed',
+    'company': 'Mahmoud Ahmed',
+    'website': 'https://www.mahmoud.com',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
